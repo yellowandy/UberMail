e48286d52262fad966794b92e9a4ff62fc03b7ef
Andreas Sandberg <andreas.a.sandberg@gmail.com>
2013-04-19 22:14:24 -0700

    Initial Commit

---
+++ UberMail/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UberMail
+//
+//  Created by Amanda Sandberg on 4/19/13.
+//  Copyright (c) 2013 Andreas Sandberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LQBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LQBAppDelegate class]));
+    }
+}
